1fe44f96bd21fd1442340c4fa06a883bb1807c7e
Nikki Sato <nikki.satopdx@gmail.comgit config --global user.email nikki.satopdx@gmail.com>
2022-01-09 20:32:16 -0800

    Giant Letter

--- sketches/sketch-04.js
+++ sketches/sketch-04.js
@@ -52,7 +52,6 @@ const sketch = () => {
 
       // const n = random.noise2D(x + frame * 50, y, params.freq);
       const n = random.noise3D(x, y, frame * 10, params.freq);
-
       const angle = n * Math.PI * params.amp;
 
       // const scale = ((n + 1) / 2) * 30;
